92bd99756f772a3699db6d0ce3619512d27e8802
phj233 <2780990934@qq.com>
2023-03-29 22:40:16 +0800

    设置发送到客户端的字符编码,用户退出时清空token

--- src/main/java/info/phj233/onlinechat/handler/LogoutSuccessHandlerImpl.java
+++ src/main/java/info/phj233/onlinechat/handler/LogoutSuccessHandlerImpl.java
@@ -1,12 +1,14 @@
 package info.phj233.onlinechat.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import info.phj233.onlinechat.config.JWTConfig;
 import info.phj233.onlinechat.util.ResultUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -28,7 +30,12 @@ public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
     ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        if (authentication != null ){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        response.setCharacterEncoding("UTF-8");
         SecurityContextHolder.clearContext();
+        response.setHeader(JWTConfig.tokenHeader, "");
         response.getWriter().write(
                 objectMapper.writeValueAsString(
                         ResultUtil.success("退出成功")
