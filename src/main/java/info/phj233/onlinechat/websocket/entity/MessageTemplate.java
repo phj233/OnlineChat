4f0c69afd463d46afd26ba1bfe17f440f44ac98d
phj233 <2780990934@qq.com>
2023-06-23 14:02:01 +0800

    ✨ feat: Deserialize the JSON string of MessageTemplate

--- src/main/java/info/phj233/onlinechat/websocket/entity/MessageTemplate.java
+++ src/main/java/info/phj233/onlinechat/websocket/entity/MessageTemplate.java
@@ -1,8 +1,10 @@
 package info.phj233.onlinechat.websocket.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.web.socket.WebSocketSession;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
@@ -17,12 +19,13 @@ import java.util.Set;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageTemplate {
     /**
      * 消息类型
-     * 1: 私发消息
-     * 2: 群发消息 除了自己
-     * 3: 系统消息 上下线通知
+     * <br> 1: 私发消息
+     * <br> 2: 群发消息 除了自己
+     * <br> 3: 系统消息 上下线通知
      */
     Integer type;
     /**
@@ -32,9 +35,21 @@ public class MessageTemplate {
     /**
      * 消息接收者
      */
-    Set<WebSocketSession> receivers;
+    Set<String> receivers;
     /**
      * 在线人数
      */
     Integer onlineCount;
+
+    @JsonCreator
+    public MessageTemplate(@JsonProperty("type") int type,
+                           @JsonProperty("message") String message,
+                           @JsonProperty("receivers") Set<String> receivers,
+                           @JsonProperty("onlineCount") int onlineCount) {
+
+        this.type = type;
+        this.message = message;
+        this.receivers = receivers;
+        this.onlineCount = onlineCount;
+    }
 }
